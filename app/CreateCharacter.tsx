c6f2ba88c84e723438e36cfada9908b3354ff65c
Flooooooooooorian <florian.weber@neuefische.de>
2026-03-05 10:35:44 +0100

    feat: image picker

--- app/CreateCharacter.tsx
+++ app/CreateCharacter.tsx
@@ -1,9 +1,30 @@
-import {Text, TextInput, View, StyleSheet, KeyboardAvoidingView, Platform} from "react-native";
-import {useState} from "react";
+import {Text, TextInput, Image, StyleSheet, KeyboardAvoidingView, Platform, Pressable} from "react-native";
+import {useEffect, useState} from "react";
+import * as ImagePicker from "expo-image-picker";
 
 export default function CreateCharacter() {
 
     const [value, setValue] = useState<string>();
+    const [image, setImage] = useState<string>();
+    const permissions = ImagePicker.useCameraPermissions();
+
+
+    const handleImagePicker = async () => {
+        const cameraPermissions = await ImagePicker.getCameraPermissionsAsync()
+        if (cameraPermissions.granted === false) {
+            const requestResult = await ImagePicker.requestCameraPermissionsAsync()
+            if (requestResult.granted === false) {
+                return
+            }
+        }
+
+
+        const result = await ImagePicker.launchCameraAsync();
+        console.log(result)
+        if (result.canceled === false) {
+            setImage(result.assets[0].uri)
+        }
+    }
 
     return (
         <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : "height"}>
@@ -13,6 +34,12 @@ export default function CreateCharacter() {
                        style={styles.input}
                        keyboardType='numeric'
             />
+            <Pressable onPress={handleImagePicker}>
+                <Text>
+                    Image
+                </Text>
+            </Pressable>
+            <Image source={{uri: image}} style={{height: 100, width: 100}}/>
         </KeyboardAvoidingView>
 
     )
